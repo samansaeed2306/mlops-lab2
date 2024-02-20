321a9d88f3668755c709df889986e0d91274f5cd
openai-exe <sabeensaeed06@gmail.com>
2024-02-20 11:23:41 +0500

    Add all the files

---
+++ main.py
@@ -0,0 +1,24 @@
+class mlops:
+        def __init__(self, totalStudents):
+                self.totalStudents = totalStudents
+
+        def getTotalStudents(self):
+                return self.totalStudents
+        
+        def addStudent(self):
+                self.totalStudents=self.totalStudents + 1
+        
+        def removeStudent(self):
+                self.totalStudents=self.totalStudents - 1
+        
+        def getClassName(self):
+                return "Machine Learning Operations (CS-B)"
+        
+mlops_class = mlops(5)
+
+mlops_class.addStudent()
+
+mlops_class.removeStudent()
+
+print(mlops_class.getTotalStudents())  # Call the method
+print(mlops_class.getClassName())      # Call the method
